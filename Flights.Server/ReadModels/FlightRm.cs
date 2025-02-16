f2a988354ac5d87fbdea2ed7eb8b061a2a82326f
Gideon Isa <innocentgideon10@gmail.com>
2025-02-16 19:43:07 +0100

    feature/add-flight-controller

---
+++ Flights.Server/ReadModels/FlightRm.cs
@@ -0,0 +1,12 @@
+﻿namespace Flights.Server.ReadModels
+{
+    public record FlightRm(
+        Guid Id, 
+        string Airline, 
+        string Price, 
+        TimePlaceRm Departure, 
+        TimePlaceRm Arrival, 
+        int RemainingNumberOfSeats
+        );
+
+}
